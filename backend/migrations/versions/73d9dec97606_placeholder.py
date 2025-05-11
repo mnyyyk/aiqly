11ba552b96825e0e1b59f11934787708bd84f745
Yosuke Kohno <yosuke.kohno@mnyy.jp>
2025-05-11 15:09:48 +0000

    chore(migrations): add placeholder for lost revision 73d9dec97606

---
+++ backend/migrations/versions/73d9dec97606_placeholder.py
@@ -0,0 +1,14 @@
+from alembic import op
+import sqlalchemy as sa
+revision = '73d9dec97606'
+down_revision = '57a06407e7df'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
